576068c220e077a9f2945a1a0782db5a525ebded
bellarann <131433170+bellarann@users.noreply.github.com>
2023-05-10 20:16:38 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+enum RadioMessage {
+    message1 = 49434
+}
+radio.onReceivedMessage(RadioMessage.message1, function () {
+    music.playMelody("E B E B E B E B ", 307)
+    for (let index = 0; index < 10; index++) {
+        basic.showIcon(IconNames.Diamond)
+        basic.showIcon(IconNames.SmallDiamond)
+    }
+})
+radio.setGroup(2)
 basic.forever(function () {
-	
+    if (input.lightLevel() > 50) {
+        radio.sendMessage(RadioMessage.message1)
+    }
 })
