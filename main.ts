553d6ba85fed937fa3a8c556fbda90692d81c119
bellarann <131433170+bellarann@users.noreply.github.com>
2023-05-12 08:45:33 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,9 @@ enum RadioMessage {
     message1 = 49434,
     alarm = 2438
 }
-radio.onReceivedMessage(RadioMessage.alarm, function () {
-    music.playMelody("E B E B E B E B ", 376)
-    for (let index = 0; index < 10; index++) {
-        basic.showIcon(IconNames.Diamond)
-        basic.showIcon(IconNames.SmallDiamond)
-    }
-})
 radio.setGroup(8148)
 basic.forever(function () {
     if (input.lightLevel() > 50) {
-        radio.sendMessage(RadioMessage.alarm)
+        radio.sendNumber(0)
     }
 })
