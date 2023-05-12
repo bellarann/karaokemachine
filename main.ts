f46fa68fb8b6d5d45981c4110926f172464b6d9b
bellarann <131433170+bellarann@users.noreply.github.com>
2023-05-12 08:18:50 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,17 @@
 enum RadioMessage {
-    message1 = 49434
+    message1 = 49434,
+    alarm = 2438
 }
-radio.onReceivedMessage(RadioMessage.message1, function () {
-    music.playMelody("E B E B E B E B ", 307)
+radio.onReceivedMessage(RadioMessage.alarm, function () {
+    music.playMelody("E B E B E B E B ", 376)
     for (let index = 0; index < 10; index++) {
         basic.showIcon(IconNames.Diamond)
         basic.showIcon(IconNames.SmallDiamond)
     }
 })
-radio.setGroup(2)
+radio.setGroup(8148)
 basic.forever(function () {
     if (input.lightLevel() > 50) {
-        radio.sendMessage(RadioMessage.message1)
+        radio.sendMessage(RadioMessage.alarm)
     }
 })
